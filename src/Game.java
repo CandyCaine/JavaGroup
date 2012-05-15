6b4fb830ef73b357c755ded923a5f24b7dd2d87f
unknown <Administrator@XenonForce.(none)>
2012-05-14 19:36:12 -0500

    Bill gates my friend

--- src/Game.java
+++ src/Game.java
@@ -1,21 +1,12 @@
-
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 import com.javagroup.game.graphics.BasicGame;
-import com.javagroup.game.graphics.entity.testEnt;
 import com.javagroup.game.graphics.image.Art;
 import com.javagroup.game.graphics.image.Font;
-<<<<<<< HEAD
 import com.javagroup.game.graphics.particle.Emitter;
 import com.javagroup.game.graphics.particle.ParticleSystem;
-import com.javagroup.game.graphics.particle.emitter.BloodEmitter;
-=======
-import com.javagroup.game.graphics.image.SpriteSheet;
-import com.javagroup.game.graphics.particle.Effect;
-import com.javagroup.game.graphics.particle.Effect.EffectType;
->>>>>>> 6e932999b75df71095b4700e1455ea519e252969
 import com.javagroup.game.input.Input;
 import com.javagroup.game.map.Map;
 
@@ -25,34 +16,31 @@ public class Game extends BasicGame {
 	float x = 450;
 	float y;
 
+	ParticleSystem particleSystem;
+	Emitter emitter, emitter2, emitter3;
+	Font font = new Font();
 	BufferedImage testFont;
-<<<<<<< HEAD
-	testEnt testent;
-
-	
-=======
-	Animation testanim;
-	SpriteSheet testSheet;
-	
-	private Effect effects;
->>>>>>> 6e932999b75df71095b4700e1455ea519e252969
 
 	@Override
 	public void initiate() {
 		Art.init();
-		testFont = Font.getFont().getLetters("PARTICLES");
-<<<<<<< HEAD
-		testent = new testEnt();
-=======
-		effects = new Effect();
->>>>>>> 6e932999b75df71095b4700e1455ea519e252969
-		
-
-		
+		testFont = font.getLetters("PARTICLES");
 		// Testing
 		{
 			testMap = new Map("Test", 32, 32);
-			Effect.playEffect(EffectType.BloodEffect, 100, 200);
+			particleSystem = new ParticleSystem();
+			emitter = new Emitter(1000, 2.5F, 100);
+			emitter.setLocation(-300, 250);
+			emitter.setImage(Art.getTileSheet().getImage(64, 32, 8, 8));
+			particleSystem.addEmitter(emitter);
+			emitter2 = new Emitter(1000, 2.0F, 100);
+			emitter2.setLocation(-300, 250);
+			emitter2.setImage(Art.getTileSheet().getImage(72, 32, 8, 8));
+			particleSystem.addEmitter(emitter2);
+			emitter3 = new Emitter(1000, 1.5F, 100);
+			emitter3.setLocation(-300, 250);
+			emitter3.setImage(Art.getTileSheet().getImage(80, 32, 8, 8));
+			particleSystem.addEmitter(emitter3);
 		}
 
 	}
@@ -60,13 +48,8 @@ public class Game extends BasicGame {
 	@Override
 	public void render(Graphics2D g) {
 		testMap.render(g, Math.round(x), Math.round(y));
+		particleSystem.render(g);
 		g.drawImage(testFont, Math.round(x) + (-350), 100, testFont.getWidth(), testFont.getHeight(), null);
-<<<<<<< HEAD
-		testent.render(g);
-=======
-		g.drawImage(testanim.getImage(),30,30,null);
-		effects.render(g);
->>>>>>> 6e932999b75df71095b4700e1455ea519e252969
 	}
 
 	@Override
@@ -83,13 +66,7 @@ public class Game extends BasicGame {
 			x += 0.1F * delta;
 		}
 
-<<<<<<< HEAD
 		particleSystem.update(x, y);
-		testent.update(delta);
-=======
-		testanim.update();
-		effects.update(x, y);
->>>>>>> 6e932999b75df71095b4700e1455ea519e252969
 
 	}
 
@@ -97,4 +74,5 @@ public class Game extends BasicGame {
 		Game game = new Game();
 		game.createWindow();
 	}
+
 }
