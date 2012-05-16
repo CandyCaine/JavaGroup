e7ae1b399d13114f9767a1471452da6aebe60a4a
Randy <Administrator@XenonForce.(none)>
2012-05-15 22:42:17 -0500

    trying

--- src/com/javagroup/game/map/Tile.java
+++ src/com/javagroup/game/map/Tile.java
@@ -5,21 +5,30 @@ import com.javagroup.game.graphics.image.Texture;
 
 public class Tile {
 
-	public static enum TileType {
-		Grass		(0),
-		Rock		(1),
-		Rocks		(2),
-		Pond		(3),
-		Mushroom	(4);
+	public static enum TileType{
+		
+		Rock		(1, true),
+		Rocks		(2, true),
+		Pond		(3, false),
+		Mushroom	(4, false),
+		Grass		(5, false);
 		
 		private int id;
-		TileType(int id) {
+		private boolean collidable;
+		TileType(int id, boolean collidable) {
+			System.out.println("id");
 			this.id = id;
+			this.collidable = collidable;
+			
 		}
 		
 		public int getID() {
 			return id;
 		}
+		
+		public boolean isCollidable(){
+			return collidable;
+			}
 	}
 	
 	protected float x;
@@ -27,12 +36,65 @@ public class Tile {
 	protected int id;
 	private Texture texture;
 	private TileType type;
+	private boolean collideable;
 	
 	public Tile(int id, float x, float y) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
+		
 		texture = Art.getTextureByID(id);
+		
+		
+	
+		setType(id);
+		
+	}
+	
+	private void setType(int id){
+		
+		
+		for(int i = 1; i <=5; i++){
+			
+			if(id == 1){
+			type = TileType.Rock;
+			
+			}
+			if(id == 2){
+			type = TileType.Rocks;
+			}
+			if(id == 3){
+			 type = TileType.Pond;
+			}
+			if(id == 4){
+			type = TileType.Mushroom;
+			}
+			if(id == 5){
+			type = TileType.Grass;
+			}
+			
+			
+		
+			
+			if(type == null){
+				System.out.println("Im null");
+				
+				return;
+			}
+			
+		
+			
+			if(type!= null){
+				collideable = type.isCollidable();
+				
+				System.out.println("i'm not null");
+			}
+			
+			}
+		
+		
+		
+	
 	}
 	
 	public void setID(int id) {
@@ -68,4 +130,15 @@ public class Tile {
 	public Texture getTexture() {
 		return texture;
 	}
+	
+	public TileType  getTileType(){
+		return type;
+	}
+	
+	public boolean isCollideable(){
+		return collideable;
+		
+	}
+	
+
 }
